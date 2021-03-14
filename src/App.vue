f14d74031005fb610f5702de3ce17ab53162451a
Roney Berti <33360638+roneyword@users.noreply.github.com>
2021-03-14 13:40:18 -0300

    add arquivos

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Button
+      label="my button"
+      typeButton="submite"
+      :outline="false"
+      :block="false"
+      :isDisable="false"
+      :loading="false"
+      :icon="true"
+    >
+    </Button>
+  </div>
+</template>
+
+<script>
+import Button from './components/ShipButton.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Button
+  }
+}
+</script>
---
+++ src/stories/lib/teste/Teste.stories.js
@@ -0,0 +1,20 @@
+import Button from "../../../components/shipButton.vue";
+
+export default {
+  title: "Ship Button",
+  component: Button,
+  argTypes: {
+    label: String,
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { Button },
+  template: '<Button @onClick="onClick" v-bind="$props" />',
+});
+
+export const TesteButton = Template.bind({});
+TesteButton.args = {
+  label: "Ship Button",
+};
